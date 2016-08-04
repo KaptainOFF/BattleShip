42787dc28c3865344a6b4726498bdaf2e2acf43a
yani.kapitanov <yani.kapitanov@gmail.com>
2016-08-04 12:01:45 +0100

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,16 @@ public class Main {
 
     public static void main(String[] args) {
         Game game = new Game();
-        game.printBoard();
-        game.getPlayerRow();
-        game.getPlayerCol();
+        do {
+            game.printBoard();
+            game.getPlayerCol();
+            game.getPlayerRow();
+
+            if (game.getPlayerCol() != game.getRandCol() && game.getPlayerRow() != game.getRandRow()) {
+                game.mBoard[game.getPlayerCol()][game.getPlayerRow()] = "X";
+                game.printBoard();
+            }
+            tries++;
+        } while (game.getPlayerCol() != game.getRandCol() && game.getPlayerRow() != game.getRandRow() || tries < 5);
     }
 }
