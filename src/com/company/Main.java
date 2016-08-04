6b0711728bfef07dfb004a3f050a4afd99ca7c97
yani.kapitanov <yani.kapitanov@gmail.com>
2016-08-04 13:57:14 +0100

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,14 +2,18 @@ package com.company;
 
 public class Main {
     private static int tries = 0;
+
     public static void main(String[] args)  {
         Game game = new Game();
 
         while (tries < 5) {
-            game.getPlayerCol();
-            game.getPlayerRow();
-            if (game.getPlayerCol() != game.getRandCol() && game.getPlayerRow() != game.getRandRow()) {
-                game.mBoard[game.getPlayerCol()][game.getPlayerRow()] = "X";
+            int playerC = game.getPlayerCol();
+            int playerR = game.getPlayerRow();
+            if (((playerC - 1) == game.getRandCol()) && ((playerR - 1) == game.getRandRow())) {
+                System.out.println("You Win!");
+                break;
+            } else {
+                game.mBoard[playerC][playerR] = "X";
                 game.printBoard();
             }
             tries++;
