295a538a4b4c448b0295ad27d335263c16c74433
Y.Kapitanov <yani.kapitanov@gmail.com>
2016-08-05 08:08:14 +0100

    Check if the user has already tried those coordinates

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
     private static int tries = 0;
 
@@ -9,15 +11,23 @@ public class Main {
         while (tries < 5) {
             int playerC = game.getPlayerCol();
             int playerR = game.getPlayerRow();
+
+
             if ((playerC == game.getRandCol()) && (playerR == game.getRandRow())) {
                 System.out.println("You Win!");
                 break;
+            } else if (game.mBoard[playerC - 1 ][playerR -1 ] == "X"){
+                System.out.println("You have already guessed here!");
+                System.out.printf("You have %d tries left.", tries);
+                System.out.println();
             } else {
                 game.mBoard[playerC - 1][playerR - 1] = "X";
                 game.printBoard();
+                tries++;
+                System.out.printf("You have %d tries left.", tries);
+                System.out.println();
             }
-            tries++;
         }
-        System.out.println("Sorry, you lose!");
+        System.out.println("Game Over");
     }
 }
