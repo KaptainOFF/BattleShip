51a6e247fe2b37fc27939bdeb65b7139db9bb922
yani.kapitanov <yani.kapitanov@gmail.com>
2016-08-05 17:33:22 +0100

    Fixed try catch

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.sun.org.apache.xerces.internal.impl.dv.InvalidDatatypeValueException;
+
 import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
@@ -42,36 +44,28 @@ public class Game {
         checks if the input is a number and if it's in the desired range.
         if not it will ask again
      */
-    public int getPlayerRow() {
+    public int getPlayerRow() throws InputMismatchException {
         int playerRow = 0;
-        try {
             System.out.printf("Horizontal: ");
             playerRow = scanner.nextInt();
             while (playerRow < 1 || playerRow > 5) {
                 System.out.printf("Please enter a value between 1 and 5: ");
                 playerRow = scanner.nextInt();
             }
-        } catch(InputMismatchException ime) {
-            System.out.println("Input must be integer");
-        }
         return playerRow;
     }
     /* Get player input for column
         checks if the input is a number and if it's in the desired range.
         if not it will ask again
      */
-    public int getPlayerCol() {
+    public int getPlayerCol() throws InputMismatchException {
         int playerCol = 0;
-        try {
             System.out.printf("Vertical: ");
             playerCol = scanner.nextInt();
             while (playerCol < 1 || playerCol > 5 ) {
                 System.out.printf("Please enter a value between 1 and 5: ");
                 playerCol = scanner.nextInt();
             }
-        } catch(InputMismatchException ime) {
-            System.out.println("Input must be integer");
-        }
         return playerCol;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,39 @@
 package com.company;
 
+import java.util.InputMismatchException;
+
 public class Main {
     private static int tries = 5;
 
     public static void main(String[] args)  {
         Game game = new Game();
+        int playerC = 0;
+        int playerR = 0;
 
         while (tries > 0) {
-            int playerC = game.getPlayerCol();
-            int playerR = game.getPlayerRow();
+
+            try {
+                playerC = game.getPlayerCol();
+            } catch (InputMismatchException ime) {
+                System.out.println("Input must be a number!");
+            }
+
+            try {
+                playerR = game.getPlayerRow();
+            } catch (InputMismatchException ime) {
+                System.out.println("Input must be a number!");
+            }
 
 
             if ((playerC == game.getRandCol()) && (playerR == game.getRandRow())) {
                 System.out.println("You Win!");
                 break;
+
             } else if (game.mBoard[playerC - 1 ][playerR -1 ] == "X"){
                 System.out.println("You have already guessed here!");
                 System.out.printf("You have %d tries left.", tries);
                 System.out.println();
+
             } else {
                 game.mBoard[playerC - 1][playerR - 1] = "X";
                 game.printBoard();
